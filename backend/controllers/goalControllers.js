847a6e975ca58edf216fec81172907f0b6d90f37
mohammadhani110 <mohammadhani110@gmail.com>
2022-02-12 18:23:11 +0500

    Setup Routes and Controller

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -2,13 +2,13 @@ const asyncHandler=require("express-async-handler")
 //@desc Get Goals
 //@route GET /api/goals
 //@access Private
-const getGoals= asyncHandler((req,res)=>{
+const getGoals= asyncHandler(async(req,res)=>{
     res.status(200).json({message:"Get Goals"});
 })
 //@desc Set Goals
 //@route POST /api/goals
 //@access Private
-const setGoals= asyncHandler((req,res)=>{
+const setGoals= asyncHandler(async(req,res)=>{
     if(!req.body.text){
         res.status(400)
         throw new Error("Please add a text field")
@@ -18,13 +18,13 @@ const setGoals= asyncHandler((req,res)=>{
 //@desc Update Goal
 //@route PUT /api/goals
 //@access Private
-const updateGoal= asyncHandler((req,res)=>{
+const updateGoal= asyncHandler(async(req,res)=>{
     res.status(200).json({message:`Update Goal ${req.params.id}`})
 })
 //@desc Delete Goal
 //@route DELETE /api/goals
 //@access Private
-const deleteGoal= asyncHandler((req,res)=>{
+const deleteGoal= asyncHandler(async(req,res)=>{
     res.status(200).json({message:`Delete Goal ${req.params.id}`})
 })
 
